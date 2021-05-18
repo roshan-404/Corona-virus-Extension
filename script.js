718927aa58307541e561fc3396acb28800402ad2
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-05-19 01:55:56 +0530

    Hilighting the COVID-19

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ function replaceText(element) {
     } else if (element.nodeType === Text.TEXT_NODE) {
         if (element.textContent.match(/covid-19/gi)) {
             const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(covid-19)/gi, '<span style =" background-color: black; color: black;">$1</span>')
+            newElement.innerHTML = element.textContent.replace(/(covid-19)/gi, '<span class="rainbow">$1</span>')
             element.replaceWith(newElement)
+
+            // element.parentElement.remove()
         }
         // element.textContent = element.textContent.replace(/coronavirus/gi, '')
+        
     }
 }
